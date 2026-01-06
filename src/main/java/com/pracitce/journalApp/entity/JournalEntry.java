6c34c00885f4a8c1e2b71d0827e27265962413cb
byte-ranajit <ranajitkhandual@gmail.com>
2026-01-06 19:54:50 +0530

    no changes

--- src/main/java/com/pracitce/journalApp/entity/JournalEntry.java
+++ src/main/java/com/pracitce/journalApp/entity/JournalEntry.java
@@ -3,9 +3,7 @@ package com.pracitce.journalApp.entity;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
-
 import java.time.LocalDateTime;
 
 @Data
